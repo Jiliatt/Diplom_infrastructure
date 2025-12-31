7bf6efb1594bc980f36ef1c3e8da030476000680
Valeriy <richcompanyv@gmail.com>
2025-12-31 14:38:53 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "yandex" {
+  token     = "$yc iam create-token"   # САМ ВСТАВЛЯЕШЬ СЮДА СВОЙ ТОКЕН ДЛИННЫЙ 
   folder_id = "b1g2dh5hul1tiuhpl1bk"
   zone      = "ru-central1-b"
 }
